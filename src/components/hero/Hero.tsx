b5ccadfbc26bcc9fe831e4268bd86ec1fcff7f69
mightGuy <onyejemeemmanuel65@gmail.com>
2023-02-15 05:16:39 +0100

    Fixed Hero height issue with landscape mode

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -6,9 +6,9 @@ function Hero() {
     return (
 
         <div className="text-white">
-            <div className="w-full h-screen max-w-[800px] mx-auto mt-[-96px] px-4 text-center flex flex-col justify-center items-center">
+            <div className="w-full h-screen min-h-[800px] max-w-[800px] mx-auto mt-[-96px] px-4 text-center flex flex-col justify-center items-center">
                 <p className="uppercase text-[#00df9a] font-bold p-2">Growing with data analytics</p>
-                <h1 className="font-bold text-4xl sm:text-6xl md:text-7xl md:py-6">Grow with data.</h1>
+                <h1 className="font-bold text-4xl sm:text-6xl md:text-7xl m-2 md:py-6">Grow with data.</h1>
                 <div className='flex justify-center items-center gap-2'>
                     <p className="font-bold py-4 text-xl sm:text-4xl md:text-5xl">Fast, flexible financing for </p>
                     <Typed
@@ -20,7 +20,7 @@ function Hero() {
                     />
 
                 </div>
-                <p className='text-xl font-bold text-gray-500 md:text-2xl' >Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
+                <p className='text-xl font-bold text-gray-500 mt-2 md:mt-6 md:text-2xl' >Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
                 {/* <button className='bg-[#00df9a] py-2.5 px-12 text-[#333333] font-bold cursor-pointer mt-4 rounded-md'>Get Started</button> */}
                 <Button>Get Started</Button>
             </div>
